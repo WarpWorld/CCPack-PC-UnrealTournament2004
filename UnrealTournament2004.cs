7383e1db10fcd303acb9557772084ccb2d020f31
Crowd Control Effect Pack Synchronization Tool <kat@warp.world>
2024-04-24 16:57:03 +0000

    Crowd Control Effect Pack Synchronization Tool - Automated Commit

--- UnrealTournament2004.cs
+++ UnrealTournament2004.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using CrowdControl.Common;
-using CrowdControl.Games.Packs;
+﻿using CrowdControl.Common;
 using ConnectorType = CrowdControl.Common.ConnectorType;
 
 namespace CrowdControl.Games.Packs.UnrealTournament2004;
